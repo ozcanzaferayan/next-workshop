23014aff31a678455cb09f1a213685e8283f6d12
Zafer AYAN <ozcanzaferayan@gmail.com>
2024-05-05 20:38:15 +0300

    Add logger to drizzle

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,21 @@
 let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+import { env } from "@/env.mjs";
 import { neon } from "@neondatabase/serverless";
+import { type Logger } from "drizzle-orm/logger";
 import { drizzle } from "drizzle-orm/neon-http";
 
-const sql = neon(
-  `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`
-);
+class QueryLogger implements Logger {
+  logQuery(query: string, params: unknown[]): void {
+    console.debug("___QUERY___");
+    console.debug(query);
+    console.debug(params);
+    console.debug("___END_QUERY___");
+  }
+}
 
-const db = drizzle(sql);
+const sql = neon(env.DATABASE_URL);
+
+const db = drizzle(sql, { logger: new QueryLogger() });
 
 export * from "./schema";
 
